f5814a778ce345bfc8eb5fbb47980ab65b08a818
Alikemalsari <a.kemalsari46@gmail.com>
2022-12-11 20:28:40 +0300

    started to be developed oyuncu vs bilgisayar page
    started to be  developed oyuncu vs bilgisayar page

--- my-react-project/src/OyuncuBilgisayar.js
+++ my-react-project/src/OyuncuBilgisayar.js
@@ -1,8 +1,49 @@
-function OyuncuBilgisayar(){
-    return(
-        <div>
-            Oyuncu Bilgisayar Ekranı
+import "./App.css";
+
+import { useState } from "react";
+
+
+function Player({ name = "Player", score = 0, action = "rock" }) {
+    return (
+        <div className="player">
+            <div className="score">{`${name}: ${score}`}</div>
+            <div className="action">
+                {action && <ActionIcon action={action} size={60} />}
+            </div>
         </div>
-    )
-};
+    );
+}
+
+function OyuncuBilgisayar() {
+    const [playerAction, setPlayerAction] = useState("");
+    const [computerAction, setComputerAction] = useState("");
+
+    const [playerScore, setPlayerScore] = useState(0);
+    const [computerScore, setComputerScore] = useState(0);
+    const [winner, setWinner] = useState(0);
+
+    return (
+        <div className="center">
+            <h1>Oyuncu VS Bilgisayar</h1>
+            <div>
+                <div className="container">
+                    <Player name="1.Oyunucu" score={playerScore} action={playerAction} />
+                    <Player
+                        name="Bilgisayar"
+                        score={computerScore}
+                        action={computerAction}
+                    />
+                </div>
+                <div>
+                    <ActionButton action="rock" />
+                    <ActionButton action="paper" />
+                    <ActionButton action="scissors" />
+
+                </div>
+                <ShowWinner winner={winner}/>
+            </div>
+        </div>
+    );
+}
+
 export default OyuncuBilgisayar;
