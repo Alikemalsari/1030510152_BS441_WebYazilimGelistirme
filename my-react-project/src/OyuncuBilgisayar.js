f117e0c2ed403963915ec04241e8135845e912ae
Alikemalsari <a.kemalsari46@gmail.com>
2022-12-18 22:34:00 +0300

    continued to be developed oyuncu vs bilgisayar page
    continued to be developed oyuncu vs bilgisayar page

--- my-react-project/src/OyuncuBilgisayar.js
+++ my-react-project/src/OyuncuBilgisayar.js
@@ -1,7 +1,45 @@
 import "./App.css";
+import { FaHandRock, FaHandPaper, FaHandScissors } from "react-icons/fa";
+import { useState} from "react";
 
-import { useState } from "react";
+const actions = {
+    rock: ["scissors", "lizard"],
+    paper: ["rock", "spock"],
+    scissors: ["paper", "lizard"],
+  
+};
 
+function randomAction() {
+    const keys = Object.keys(actions);
+    const index = Math.floor(Math.random() * keys.length);
+
+    return keys[index];
+}
+
+function calculateWinner(action1, action2) {
+    if (action1 === action2) {
+        return 0;
+    } else if (actions[action1].includes(action2)) {
+        return -1;
+    } else if (actions[action2].includes(action1)) {
+        return 1;
+    }
+
+ 
+    return null;
+}
+
+function ActionIcon({ action, ...props }) {
+    const icons = {
+        rock: FaHandRock,
+        paper: FaHandPaper,
+        scissors: FaHandScissors,
+        //lizard: FaHandLizard,
+        //spock: FaHandSpock,
+    };
+    const Icon = icons[action];
+    return <Icon {...props} />;
+}
 
 function Player({ name = "Player", score = 0, action = "rock" }) {
     return (
@@ -14,6 +52,25 @@ function Player({ name = "Player", score = 0, action = "rock" }) {
     );
 }
 
+function ActionButton({ action = "rock", onActionSelected }) {
+    return (
+        <button className="round-btn" onClick={() => onActionSelected(action)}>
+            <ActionIcon action={action} size={20} />
+        </button>
+    );
+}
+
+function ShowWinner({winner = 0}) {
+    const text = {
+        "-1": "Tebrikler, KAZANDINIZ!",
+        0: "Berabere...",
+        1: "KAYBETTİNİZ!!",
+    };
+    return (
+        <h2>{text[winner]}</h2>
+    )
+}
+
 function OyuncuBilgisayar() {
     const [playerAction, setPlayerAction] = useState("");
     const [computerAction, setComputerAction] = useState("");
@@ -22,12 +79,33 @@ function OyuncuBilgisayar() {
     const [computerScore, setComputerScore] = useState(0);
     const [winner, setWinner] = useState(0);
 
+    const [player1name, setPlayer1name] = useState("");
+
+
+    const onActionSelected = (selectedAction) => {
+        const newComputerAction = randomAction();
+
+        setPlayerAction(selectedAction);
+        setComputerAction(newComputerAction);
+
+
+
+        const newWinner = calculateWinner(selectedAction, newComputerAction);
+        setWinner(newWinner);
+        if (newWinner === -1) {
+            setPlayerScore(playerScore + 1);
+        } else if (newWinner === 1) {
+            setComputerScore(computerScore + 1);
+        }
+    };
+
+   
     return (
         <div className="center">
             <h1>Oyuncu VS Bilgisayar</h1>
             <div>
                 <div className="container">
-                    <Player name="1.Oyunucu" score={playerScore} action={playerAction} />
+                    <Player name={player1name} score={playerScore} action={playerAction} />
                     <Player
                         name="Bilgisayar"
                         score={computerScore}
@@ -35,9 +113,9 @@ function OyuncuBilgisayar() {
                     />
                 </div>
                 <div>
-                    <ActionButton action="rock" />
-                    <ActionButton action="paper" />
-                    <ActionButton action="scissors" />
+                    <ActionButton action="rock" onActionSelected={onActionSelected} />
+                    <ActionButton action="paper" onActionSelected={onActionSelected} />
+                    <ActionButton action="scissors" onActionSelected={onActionSelected} />
 
                 </div>
                 <ShowWinner winner={winner}/>
